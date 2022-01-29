0e8be8194f96089fed42e88ef23dfb1598180ee4
Marc Gusmano <mfusmano@yahoo.com>
2022-01-29 14:51:43 -0500

    more

--- src/index.js
+++ src/index.js
@@ -4,42 +4,36 @@ import { HashRouter } from "react-router-dom";
 //import App from './App';
 import CardReport from './CardReport';
 
+// import reactToWebComponent from 'react-to-webcomponent';
+// const Index = (props) =>{
+//   var Partner = JSON.parse(sessionStorage.getItem('Partner')); 
+//   return (
+//     <React.StrictMode>
+//       <HashRouter>
+//       <CardReport Partner={Partner}/>
+//       </HashRouter> 
+//     </React.StrictMode>
+//   )
+// };
+// customElements.define('card-report', reactToWebComponent(Index, React, ReactDOM));
 
-
-import reactToWebComponent from 'react-to-webcomponent';
-const Index = (props) =>{
-  var Partner = JSON.parse(sessionStorage.getItem('Partner')); 
-  return (
-    <React.StrictMode>
-      <HashRouter>
-      <CardReport Partner={Partner}/>
-      </HashRouter> 
-    </React.StrictMode>
-  )
-};
-customElements.define('card-report', reactToWebComponent(Index, React, ReactDOM));
-
-// var Partner = { //CNA
-//   PartnerID: 395,
-//   PartnerShort: 'CNA',
-//   PartnerName: 'CNA',
-//   PersonID: 275399,
-//   GroupID: 33582,
-//   //showratings: false,
-//   //ratingsources: '4', //ManagerRating
-//   //SMEOnly: true,
-//   //showlob: false,
-//   showskills: false,
-//   reportName: 'Risk Control Skills Report',
-//   image: './images/CNA.png',
-//   largerButton: false
-// }
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <HashRouter>
-//        {/* <App /> */}
-//        <CardReport Partner={Partner}/>
-//     </HashRouter> 
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+var Partner = { //CNA
+  PartnerID: 395,
+  PartnerShort: 'CNA',
+  PartnerName: 'CNA',
+  PersonID: 275399,
+  GroupID: 33582,
+  showskills: false,
+  reportName: 'Risk Control Skills Report',
+  image: './images/CNA.png',
+  largerButton: false
+}
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
+       {/* <App /> */}
+       <CardReport Partner={Partner}/>
+    </HashRouter> 
+  </React.StrictMode>,
+  document.getElementById('root')
+);
